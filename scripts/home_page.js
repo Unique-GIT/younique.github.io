cb47a56c12477c9b16ec62c958f64ca5c9edde0b
Aakash <aakash04unique@gmail.com>
2025-12-24 18:39:48 +0530

    Minor changes to home page

--- scripts/home_page.js
+++ scripts/home_page.js
@@ -1,9 +1,9 @@
-
-const path = "../Blogs_html/About Me.html"
-
 export async function onLoad(){
+    const path = "../Blogs_html/About%20Me.html"
     let mainDoc = document.querySelector(".homepage-main-doc")
     let content = await fetch(path)
     let html = await content.text()
+    console.log("This is the home page file fetched:  ")
+    console.log(html)
     mainDoc.innerHTML = html
 }
